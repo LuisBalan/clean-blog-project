f9583fc628dad7e130839e5d283d80582b87b0ea
LuisBalan <jlbalcz@gmail.com>
2022-02-25 22:40:09 -0600

    first commit: first configuration

---
+++ index.js
@@ -0,0 +1,30 @@
+const { application } = require('express');
+const express = require('express');
+const res = require('express/lib/response');
+const path = require('path');
+
+const app = new express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './pages/index.html'))
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './pages/about.html'))
+});
+
+app.get('/post', (request, response) => {
+    response.sendFile(path.resolve(__dirname, './pages/post.html'))
+});
+
+app.get('/contact', (request, response) => {
+    response.sendFile(path.resolve('./pages/contact.html'))
+});
+
+
+app.listen(4000, () => {
+    console.log('App listening in the port 4000')
+});
